3820dabe4f1a380ece56ef28160acdb0a11b732b
9meo <sansarn@gmail.com>
2022-10-25 07:07:21 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+requirements = []
+with open('requirements.txt', 'rt') as f:
+    for req in f.read().splitlines():
+        if req.startswith('git+'):
+            pkg_name = req.split('/')[-1].replace('.git', '')
+            if "#egg=" in pkg_name:
+                pkg_name = pkg_name.split("#egg=")[1]
+            requirements.append(f'{pkg_name} @ {req}')
+        else:
+            requirements.append(req)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyterrier-monoQA",
+    version="0.0.1",
+    author="Sarawoot Kongyoung",
+    author_email='s.kongyoung.1{at}research.glasgow.ac.uk',
+    description="PyTerrier components for monoQA ranking and Extracting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.6',
+)
